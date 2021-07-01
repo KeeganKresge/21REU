e782e6516587cbc8459eed2b38ed271874b8a624
sophm02 <som324@lehigh.edu>
2021-07-01 11:50:26 -0400

    function getting data from mat

--- get_data_from_mat.py
+++ get_data_from_mat.py
@@ -1,12 +1,17 @@
 from scipy.io import loadmat
 import pandas as pd
+
+
 # gets data from matrix
-fil_mat_arr = loadmat('fil_mat.mat')
-fil_mat_lst=[tuple([elem for elem in row]) for row in fil_mat_arr['fil_mat'][0:-1]]
-columns = []
-for gest in ["+y","+z", "read", "-y", "-z"]:
-    for suff in ["_k_", "_s_"]:
-        for num in range(1, 31):
-            columns+=[gest+suff+str(num)]
-df = pd.DataFrame(fil_mat_lst, columns=columns)
-print(df)
+
+def mat_data():
+    fil_mat_arr = loadmat('fil_mat.mat')
+    fil_mat_lst = [tuple([elem for elem in row]) for row in fil_mat_arr['fil_mat'][0:-1]]
+    columns = []
+    for gest in ["+y", "+z", "read", "-y", "-z"]:
+        for suff in ["_k_", "_s_"]:
+            for num in range(1, 31):
+                columns += [gest + suff + str(num)]
+    df = pd.DataFrame(fil_mat_lst, columns=columns)
+    return df
+
